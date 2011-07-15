3bad534174c11856b95e0487809ce13fc29c4791
Markus Mayer <onaneerde@doogvaard.net>
2011-07-15 04:03:37 +0200

    Comments revised, code refactored, improved debug mode

--- app/maat/app.php
+++ app/maat/app.php
@@ -3,37 +3,85 @@
 namespace Saft;
 
 
-Class Maat extends App {								#                 0 = off, 1 = on
-														# -- START CONFIG ---------------
-	const HISTORY = 0,									# keep track of authorship, store entry history in “/log/history”
-		  LIFESPAN = 3600,								# authentication lifespan while requesting nothing in sec.
-		  MAX_FILE_SIZE = '3M',							# max. file size for uploads, e.g. “1M” = 1 megabyte
-		  PRIVATE_KEY = '0fecd9d17806ff60f519ac441cbaa14a54802d6c';
-														# string to secure nonce encryption (digest authentication)
-														#    e.g. get the RIPEMD-160 hash of “privatekey”;
-														#    open Terminal, type: “echo -n 'privatekey' | openssl rmd160”
-														# -- END CONFIG -----------------
-	public static $authorPot,
-				  $client,
-				  $domainID,
-				  $lang,
-				  $logRoot;
+Class Maat extends App {
+											#                 0 = off, 1 = on
+	const									# -- START CONFIG -----------------
+		HISTORY = 0,						# keep track of authorship, store
+											#    entry history in /log/history
+		LIFESPAN = 3600,					# authentication lifespan while
+											#    requesting nothing in sec.
+		MAX_FILE_SIZE = '3M',				# max. file size for uploads,
+											#    e.g. 1M = 1 megabyte
+		PRIVATE_KEY = '0fecd9d17806ff60f519ac441cbaa14a54802d6c';
+											# string to secure nonce encryption
+											#    (http digest authentication),
+											#    e.g. get the RIPEMD-160 hash
+											#    of "x"; open Terminal, type:
+											#    "echo -n 'x' | openssl rmd160"
+											# -- END CONFIG -------------------
+
+		# DO NOT EDIT ANYTHING BELOW HERE, UNLESS ONE IS DOING SOMETHING CUSTOM
+
+	public static
+		$authorPot,
+		$client,
+		$domainID,
+		$lang,
+		$logRoot;
 
 
 	public function __construct($appRoot){
-		self::$propelRoot = __DIR__;					# point to maat
-		self::$assetRoot.= '/maat';
-		self::$cacheRoot.= '/maat';						# incl. scheduled ones
-		self::$logRoot = $appRoot . '/log/maat';
-		self::$baseURI = trim(App::$absolute, '/') . '/maat/' . App::$author . '/';
-		self::$baseURL = 'http://' . $_SERVER['HTTP_HOST'] . App::$absolute . 'maat/' . App::$author . '/';
-		self::$today = 99999999;						# far future date to list scheduled entries, too
-		self::$domainID = Elf::getDomainID();
 
-		require_once('elves/copilot.php');
-		require_once('elves/auth.php');
+		if (App::DEBUG_MODE === 1){
+			require_once('elves/env.php');
+			$env = new Env_Maat();
+			unset($env);
+		}
 
-		new Auth();
+		if ($this->__extend() !== 0){
+			self::$propelRoot = __DIR__;	# point to maat
+			self::$assetRoot = $appRoot . '/asset/maat';
+			self::$cacheRoot = $appRoot . '/cache/maat';
+			self::$logRoot = $appRoot . '/log/maat';
+			self::$baseURI = trim(App::$absolute, '/') . '/maat/' . App::$author . '/';
+			self::$baseURL = 'http://' . $_SERVER['HTTP_HOST'] . App::$absolute . 'maat/' . App::$author . '/';
+			self::$today = 99999999;		# let scheduled entries list
+			self::$domainID = Elf::getDomainID();
+
+			require_once('elves/copilot.php');
+			require_once('elves/auth.php');
+			new Auth();
+		}
+	}
+
+
+	# @return	string
+	#
+	#			update $rw (get rid of "maat/"; make ready for author
+	#			name check and get rid of the author name, too;
+	#			finally, trim slashes, spaces to make ready for routing)
+
+	private function __extend(){
+		$rw = substr(App::$rw, 5) . '/';
+
+		if ((App::$author = $this->__isAuthor(strtolower(Elf::strShiftFirst($rw, '/')))) !== 0)
+			App::$rw = $rw === false
+				? $rw
+				: trim($rw, ' /');
+
+		return App::$author;
+	}
+
+
+	# @param	string
+	# @return	string or integer	0 = invalid author name
+
+	private function __isAuthor($name){
+		return (empty($name) === true
+			or	preg_match('{^[\w-]+$}i', $name) === 0
+			or	is_readable(App::$root . '/app/maat/authors/' . $name . '.json') === false)
+			? 0
+			: $name;
 	}
 
 }
---
+++ app/maat/elves/env.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Saft;
+
+
+Class Env_Maat extends Env {
+
+	function __construct(){
+		parent::__construct();
+	}
+
+	protected function __checkEnvironment(){
+		self::$arr = array(
+			'directory' => array(),
+			'file' => array(),
+			'non' => array()
+		);
+		$this->__checkPerms();
+		$this->__checkPermsMaat();			# after checkPerms!
+		$this->__buildPermsMsg();
+	}
+
+
+	private function __checkPermsMaat(){
+		$root = App::$root;
+		$perms = App::$perms;
+		$permsAsset = $perms['asset'];
+		$permsAssetParts = $perms['asset_parts'];
+		$permsCache = $perms['cache'];
+
+		# app
+
+		$this->__isSecure($root . '/app/maat');
+		$arr = $this->__rglob($root . '/app/maat', '/*', '/{*.json,*.php,*.txt}');
+
+		foreach ($arr['dirs'] as $path)
+			$this->__isSecure($path);
+
+		foreach ($arr['files'] as $path)
+			$this->__isSecure($path);
+
+
+		# asset
+
+		$this->__isSecure($root . '/asset/maat', $permsAsset);
+		$arr = $this->__rglob($root . '/asset/maat', '/*', '/{*.css,*.js,*.manifest,*.woff,*.xsl}');
+
+		foreach ($arr['dirs'] as $path)
+			$this->__isSecure($path, $permsAsset, 0755);
+
+		foreach ($arr['files'] as $path)
+			$this->__isSecure($path, $permsAssetParts, 0644);
+
+
+		# cache
+
+		Elf::makeDirOnDemand($root . '/cache/maat', $permsCache);
+		$this->__isSecure($root . '/cache/maat', $permsCache);
+		$arr = $this->__rglob($root . '/cache/maat', '/*', '/{*.html,*.json,*.xml}');
+
+		foreach ($arr['dirs'] as $path)
+			$this->__isSecure($path, $permsCache);
+
+		foreach ($arr['files'] as $path)
+			$this->__isSecure($path, $permsAssetParts, 0644);
+
+
+		# log
+
+		$arr = array(
+			'/log',
+			'/log/maat',
+			'/log/maat/history'
+		);
+
+		foreach ($arr as $part){
+			Elf::makeDirOnDemand($root . $part, $permsCache);
+			$this->__isSecure($root . $part, $permsCache);
+		}
+
+		$arr = $this->__rglob($root . '/log/maat', '/*', '/{*.log,*.zip}');
+
+		foreach ($arr['dirs'] as $path)
+			$this->__isSecure($path, $permsCache);
+
+		foreach ($arr['files'] as $path)
+			$this->__isSecure($path, $permsAssetParts, 0644);
+	}
+
+}
